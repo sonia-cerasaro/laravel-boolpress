b8eb7c466067202d0e40b2927da6185771d49334
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-05-26 19:35:06 +0200

    guest show

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+  public function show(string $slug) {
+    $slug = Post::where('slug', '=', $slug)->first();
+
+    return view('guests.posts.show', compact('posts'));
+  }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeController@index')->name('index');
+Route::get('posts/{post:slug}', 'PostController@show')->name('posts.show');
 
 Auth::routes();
 
