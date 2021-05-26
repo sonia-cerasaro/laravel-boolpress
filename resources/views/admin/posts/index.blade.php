b9a8885d0b5047e7a69b8955c922b05dae879d58
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-05-26 16:59:41 +0200

    new page create

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,1 +1,28 @@
 @extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <a href="{{route('admin.posts.create')}}">
+        New Post
+      </a>
+    </div>
+  </div>
+    <div class="row justify-content-center">
+      @foreach ($posts as $post)
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-header">
+                  {{$post->title}}
+                </div>
+                <div class="card-body">
+                  {{$post->content}}
+                </div>
+            </div>
+        </div>
+      @endforeach
+    </div>
+</div>
+
+@endsection
